8225090b8d320f298dcb7b0c6f388e8e8f3ea73b
Gabriel Silva <gabrielsilva9810@gmail.com>
2022-08-14 13:27:40 -0300

    NewAtualizacao

--- src/controller/OperacoesController.java
+++ src/controller/OperacoesController.java
@@ -3,7 +3,38 @@ package controller;
 public class OperacoesController {
 
 	public OperacoesController() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
+	// ATIVIDADE 01 - SISTEMAS OPERACIONAIS I
+	public void operacaoVetor(int qtdVetor, int qtdVetorB) {
+		
+		//STRING --> TEMPO
+		String var = "a";
+		double tempoInicial = System.nanoTime();
+		
+		for (int i = 0; i < qtdVetor; i++) {
+			var = var + "a";
+		}
+		
+		double tempoFinal = System.nanoTime();
+		double tempoTotal = tempoFinal - tempoInicial;
+		tempoTotal = tempoTotal / Math.pow(10,  9);
+		System.out.println("Tempo String do Vetor --> " + tempoTotal + "s.");
+		/*-----------------------------------------------------------------------------------------*/
+		
+		//BUFFER --> TEMPO
+		StringBuffer buffer = new StringBuffer();
+		double tempoInicialB = System.nanoTime();
+		
+		for(int i = 0; i < qtdVetor ; i++) {
+			buffer.append(qtdVetor);
+		}
+		
+		double tempoFinalB = System.nanoTime();
+		double tempoTotalB = tempoFinalB - tempoInicialB;
+		tempoTotalB = tempoTotalB / Math.pow(10,  9);
+		System.out.println("Tempo Buffer do Vetor --> " + tempoTotalB + "s.");
+	}
+	
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,10 +1,19 @@
 package view;
 
+import controller.OperacoesController;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		
+		//op.concatenaString();
+		//op.concatenaBuffer();
+		
+		OperacoesController op = new OperacoesController();
+		double[] qtdVetor = new double[1000];
+		double[] qtdVetorB = new double[1000];
+		//10000 e 100000
+		
+		op.operacaoVetor(0, 0);	
+}
 }
